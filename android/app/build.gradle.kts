b7999a2d2e06cbf7ff103847c890df8cce249f55
eemreuysal <164806648+eemreuysal@users.noreply.github.com>
2025-03-15 00:17:56 +0300

    Android NDK sürümünü yükselt ve desugaring etkinleştir

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,13 @@ plugins {
 android {
     namespace = "com.example.zenvivatodo"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Yükseltilmiş NDK sürümü
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // Desugaring etkinleştirme
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -37,6 +39,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+// Desugaring bağımlılığı
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
 
 flutter {
