1d7d955eded3698ae42db1077190874fcaf9b10f
eemreuysal <164806648+eemreuysal@users.noreply.github.com>
2025-03-17 14:40:17 +0300

    Kullanılmayan dart:io import ifadesi kaldırıldı

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 // Bildirim paketi geçici olarak kaldırıldı
 // import 'package:flutter_local_notifications/flutter_local_notifications.dart';
