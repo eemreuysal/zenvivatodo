3c8e9760931de36f126fa703a79642a6a5b6513d
eemreuysal <164806648+eemreuysal@users.noreply.github.com>
2025-03-19 02:06:19 +0300

    Fix task.g.dart to add TaskDTO serialization methods

--- lib/models/task.g.dart
+++ lib/models/task.g.dart
@@ -31,3 +31,29 @@ Map<String, dynamic> _$TaskToJson(Task instance) => <String, dynamic>{
       'userId': instance.userId,
       'uniqueId': instance.uniqueId,
     };
+
+TaskDTO _$TaskDTOFromJson(Map<String, dynamic> json) => TaskDTO(
+      id: json['id'] as int?,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      date: json['date'] as String,
+      time: json['time'] as String?,
+      isCompleted: json['isCompleted'] as bool? ?? false,
+      categoryId: json['categoryId'] as int?,
+      priority: json['priority'] as int,
+      userId: json['userId'] as int,
+      uniqueId: json['uniqueId'] as String?,
+    );
+
+Map<String, dynamic> _$TaskDTOToJson(TaskDTO instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'date': instance.date,
+      'time': instance.time,
+      'isCompleted': instance.isCompleted,
+      'categoryId': instance.categoryId,
+      'priority': instance.priority,
+      'userId': instance.userId,
+      'uniqueId': instance.uniqueId,
+    };
